cb9dff4d3198589faf7ec97cbf0f627f69270494
russianaleksey <alexmamontovprofessional@gmail.com>
2019-04-10 21:58:32 -0400

    fixed reset functionality

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const pixels = 500;
-
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -9,18 +7,23 @@ function getRandomColor() {
     }
     return color;
   }
-  
 
-var userSquares = prompt("Size of grid?");
-var pixelCount = String(pixels / userSquares) + "px";
-console.log(userSquares);
-console.log(pixelCount);
 
 
 
 const container = document.getElementById('container');
-container.style.gridTemplateColumns = `repeat(${userSquares}, ${pixelCount})`;
-container.style.gridTemplateRows = `repeat(${userSquares}, ${pixelCount})`;
+
+
+function start (){
+        const pixels = 500;
+        userSquares = prompt("How large would you like the grid to be? (Please enter number)");
+        pixelCount = String(pixels / userSquares) + "px";
+        container.style.gridTemplateColumns = `repeat(${userSquares}, ${pixelCount})`;
+        container.style.gridTemplateRows = `repeat(${userSquares}, ${pixelCount})`;
+        buildGrid();
+}
+
+
 container.addEventListener('mouseover', event => {
     if(event.target.style.background !== 'white' && !event.target.id.includes('container') && !event.target.id.includes('changed')){
         event.target.style.background = getRandomColor();
@@ -30,39 +33,38 @@ container.addEventListener('mouseover', event => {
 
 
 
+const resetButton = document.getElementById('reset-button-id');
+resetButton.addEventListener('click', event => {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    start()
+
+})
+
+
 var style = document.createElement('style');
 style.type = 'text/css';
 document.getElementsByTagName('head')[0].appendChild(style);
 style = document.getElementsByTagName('style')[0];
 
-
-var uniqueID = 0;
-for(i = 1; i <= parseInt(userSquares); i++){
-    for(j = 1; j <= parseInt(userSquares); j++){
-        /*
-        var newClass = "item" + uniqueID;
-        style.innerHTML += `.${newClass} { 
-            grid-column-start: ${i};\n
-            grid-column-end: ${i+1};\n
-            grid-row-start:${j};\n
-            grid-row-end:${j+1};\n
-            background: red;
-        }` */
-        const div = document.createElement('div');
-        div.style.gridColumnStart = i;
-        div.style.gridColumnEnd = i+1;
-        div.style.gridRowStart = j;
-        div.style.gridRowEnd = j+1;
-        /* div.classList.add(newClass); */
-        container.appendChild(div);
-        /* uniqueID++;
-        
-        div.addEventListener("mouseover", function(e){
-            div.style.background = "blue"; 
-        }) */
+function buildGrid(){
+    for(i = 1; i <= parseInt(userSquares); i++){
+        for(j = 1; j <= parseInt(userSquares); j++){
+            const div = document.createElement('div');
+            div.style.gridColumnStart = i;
+            div.style.gridColumnEnd = i+1;
+            div.style.gridRowStart = j;
+            div.style.gridRowEnd = j+1;
+            container.appendChild(div);
+        }
     }
 }
 
-function resetBoard(){
-    location.reload();
-}
+let userSquares = 16;
+buildGrid();
+container.style.gridTemplateColumns = `repeat(16, 31.25px)`;
+container.style.gridTemplateRows = `repeat(16, 31.25px)`;
+
+
+
